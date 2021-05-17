c3881f088edd6883091e6e916158c2aea84b7826
Teemote <luteciorv@gmail.com>
2021-05-17 17:49:41 -0300

    Criação da navegação das páginas e sistema de busca

---
+++ SalesWebMvc/SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Busca simples
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        // Guspa por grupos
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
